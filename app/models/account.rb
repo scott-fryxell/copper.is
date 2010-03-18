4f7efb859458ec889eb87106eea397d8531f2464
unknown <pmueller@.rubicon.com>
2010-03-18 16:05:21 -0700

    Added card type and date pick lists to order _form partial

--- app/models/account.rb
+++ app/models/account.rb
@@ -12,6 +12,7 @@
 class Account < ActiveRecord::Base
   belongs_to :user
   has_one :address
+  has_many :orders
   
   attr_accessible :last_name, :first_name, :card_type, :number, :verification_value, :month, :year 
 end
