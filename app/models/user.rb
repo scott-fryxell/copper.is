4287c08de4548c7f5bf2c4e5b9c6f2da8af8740a
Forrest L Norvell <ogd@aoaioxxysz.net>
2010-03-26 19:30:26 -0700

    Unit tests now pass, although they don't do much.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,12 @@ class User < ActiveRecord::Base
   has_many :tips
   has_many :resources
   has_and_belongs_to_many :roles
-  
-  
+
   def role_symbols
     roles.map do |role|
       role.name.underscore.to_sym
-    end    
+    end
   end
-  
-  
-  attr_accessible :email, :password, :password_confirmation
 
+  attr_accessible :email, :password, :password_confirmation
 end
