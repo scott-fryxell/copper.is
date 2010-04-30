69236207e93f27484c568e607c19b90f9e97bde9
Scott Fryxell <scott@communist.biz>
2010-04-30 15:38:39 -0700

    Set up RSpec and Autospec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,22 +4,15 @@ ENV["RAILS_ENV"] = 'test'
 require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
 require 'spec/autorun'
 require 'spec/rails'
-
-# Uncomment the next line to use webrat's matchers
 require 'webrat/integrations/rspec-rails'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
 
 Spec::Runner.configure do |config|
-  # If you're not using ActiveRecord you should remove these
-  # lines, delete config/database.yml and disable :active_record
-  # in your config/boot.rb
   config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
   config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
-
+  include Webrat::Methods
 end
 
 
