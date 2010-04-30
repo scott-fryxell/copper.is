c56701a1481b805a2c644ec0becee45171576cd7
Scott Fryxell <scott@communist.biz>
2010-04-30 15:44:29 -0700

    Added example RSpec / Webrat tests to validate testing approach.

---
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -0,0 +1,5 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe UserSessionsController do
+  it "should create a new user session when given a valid email address and a correct password"
+end
--- spec/integration/login_spec.rb
+++ spec/integration/login_spec.rb
@@ -1,12 +1,21 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "Guest login" do
-  it "should succeed when Guest provides proper credentials" do     
-      visit "/"
-      click_link "login"
-      fill_in "email", :with => "test@test.com"
-      fill_in "password", :with => "test"
-      click_button "Login"
-  end
   
+  it "should display a success message when a Guest logs in with a known email address and correct password" do
+    visit "/"
+    click_link "Login"
+    fill_in "email", :with => "test@test.com"
+    fill_in "password", :with => "test"
+    click_button "Login"
+    response.should contain("Successfully logged in.")
+  end
+
+  it "should display a failure page when a Guest tries to log in with an unknown email address"
+  it "should display a failure page when a Guest tries to log in without registering first"
+  it "should display a failure page when a Guest tries to log in with a known email address but incorrect password"
+  it "should display a failure page when a Guest tries to log in with a blank (invalid) password"
+  it "should provide a link to the registration when the Guest fails to log in"
+  it "should blank out the username field when the Guest fails to log in"
+  it "should always, for security reasons, blank out the password field when the Guest fails to log in"
 end
