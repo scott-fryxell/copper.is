28821e36edeefe98cd89ea02943bd1c0e73de991
Scott Fryxell <scott@communist.biz>
2010-05-05 15:47:29 -0700

    Started handling other response paths in bookmarklet.

--- app/controllers/patronize_controller.rb
+++ app/controllers/patronize_controller.rb
@@ -1,11 +1,9 @@
 class PatronizeController < ApplicationController
 
   def dump
-    # logger.info { 'url being tipped: ' + params[:patronized] }      
-    logger.info { 'url being tipped: ' }
-    request.body.lines.each do |line|
-      logger.info { 'BLARGH: ' + line }
+    logger.info { 'url being tipped: ' + params[:patronized] }
+    respond_to do |format|
+      format.js
     end
   end
-
 end
