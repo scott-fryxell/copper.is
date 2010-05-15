1cc3730f0cfd9a6c44c4f1b83b875a63694663c4
Forrest L Norvell <ogd@aoaioxxysz.net>
2010-05-15 00:27:27 -0700

    Removed test/ (now using RSpec exclusively).

--- test/unit/account_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AccountTest < ActiveSupport::TestCase
-  def test_should_be_valid
-    assert Account.new.valid?
-  end
-end
--- test/unit/order_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class OrderTest < ActiveSupport::TestCase
-  def test_should_be_valid
-    assert Order.new.valid?
-  end
-end
