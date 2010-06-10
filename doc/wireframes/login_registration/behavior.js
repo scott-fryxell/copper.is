8c311672e6729fdf75f84d522331fc36b32c5762
Scott Fryxell <scott@communist.biz>
2010-06-09 17:05:08 -0700

    Implemented Behavior for login and registration.

---
+++ doc/wireframes/login_registration/behavior.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+ $("#registered_no").change(function() {
+   $('form').addClass("register");
+   $("input[type=submit]").attr("value","Create Account");
+ });
+ $("#registered_yes").change(function() {
+   $('form').removeClass("register");
+   $("input[type=submit]").attr("value","Log in");
+ });
+});
