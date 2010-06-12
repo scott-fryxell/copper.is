c11d592741a0d8097eca44d0d7c19bfbe0b2de4c
Forrest L Norvell <ogd@aoaioxxysz.net>
2010-06-11 22:32:58 -0700

    DRYed up controller that's only using default views for now.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,22 +1,2 @@
 class HomeController < ApplicationController
-  def index
-    
-  end
-  
-  def terms
-    
-  end
-  
-  def privacy
-    
-  end
-  
-  def contact
-    
-  end
-  
-  def subscribe
-    
-  end
-  
 end
