16a350aae9322cdfcb9619bb2bbd1653ef8bfbb2
Forrest L Norvell <ogd@aoaioxxysz.net>
2010-07-08 13:27:55 -0700

    EP-027-5: Tightened up accounts.
    - Normalized credit card type into separate column.
    - Changed account name into a single billing_name field.
    - Made clear that associated address is a billing address.
    - Added many NOT NULL constraints.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,9 +1,13 @@
 class Account < ActiveRecord::Base
   belongs_to :user
-  belongs_to :address
+  belongs_to :billing_address, :class_name => "Address", :foreign_key => "billing_address_id"
+  belongs_to :card_type
   has_many :orders
-  
-  accepts_nested_attributes_for :address, :allow_destroy => true
-  
-  attr_accessible :card_type, :last_name, :first_name, :number, :verification_value, :card_expires_on, :address_attributes
+
+  validates_presence_of :user
+  validates_presence_of :billing_address
+  validates_presence_of :billing_name
+  validates_presence_of :number
+  validates_presence_of :expires_on
+  validates_presence_of :card_type
 end
