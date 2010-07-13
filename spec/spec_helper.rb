5b6544f06d1dccb9c0a272fab866509b815e6e11
Forrest L Norvell <ogd@aoaioxxysz.net>
2010-07-13 15:17:07 -0700

    Cleaned redundant (conventional) configuration out of RSpec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,32 +10,11 @@ require 'lib/mail-test-helper'
 # include seed data before running tests (gets cleared out otherwise)
 require "#{Rails.root}/db/seeds.rb"
 
-Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
-
 Spec::Runner.configure do |config|
   config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
-  config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
 end
-
 
 Webrat.configure do |config|
   config.mode = :rails
-end
-
-module Spec::Rails::Example
-  class IntegrationExampleGroup < ActionController::IntegrationTest
-
-    def initialize(defined_description, options={}, &implementation)
-      defined_description.instance_eval do
-        def to_s
-          self
-        end
-      end
-
-      super(defined_description)
-    end
-
-    Spec::Example::ExampleGroupFactory.register(:integration, self)
-  end
 end
