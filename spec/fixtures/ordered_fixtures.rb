a1afa156a61af9d34aff3ffe095b079f6ee78535
Phil Mueller <phil@fasterlighterbetter.com>
2010-07-27 12:57:58 -0700

    Created rake tasks to load/delete fixtures in correct order.
    - New rake task db:fixtures:load_ordered will load fixtures in order specified in file spec/fixtures/ordered_fixtures.rb
    - New rake task db:fixtures:delete will drop fixtures in reverse order specified in file spec/fixtures/ordered_fixtures.rb
    - Both tasks will also load any other fixtures not listed in spec/fixtures/ordered_fixtures.rb that are present in spec/fixtures
    - Removed script script/create_test_users as the new load fixtures rake task accomplishes the same thing.

---
+++ spec/fixtures/ordered_fixtures.rb
@@ -0,0 +1,12 @@
+ENV["FIXTURE_ORDER"] = 
+  %w( users 
+      roles_users
+      addresses
+      accounts
+      transactions
+      pages
+      sites
+      locators
+      tip_bundles
+      refills
+      tips ).join(' ')
