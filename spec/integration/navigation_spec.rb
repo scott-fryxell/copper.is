dad8792a0f070d3947572ce5e9e4203f31387ae4
Phil Mueller <phil@fasterlighterbetter.com>
2010-08-03 16:58:16 -0700

    Initial implementation of signaling in account section

--- spec/integration/navigation_spec.rb
+++ spec/integration/navigation_spec.rb
@@ -1,82 +1,127 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "The standard Weave page" do
-  it "should contain a logo" do
+  before(:each) do
     visit "/"
+  end
+
+  it "should contain a logo" do
     assert_have_selector "body img[item=logo]"
   end
 
   it "should contain a global nav section" do
-    visit "/"
     assert_have_selector "body > header"
   end
 
   it "should contain an account section" do
-    visit "/"
     assert_have_selector "body > aside"
   end
   it "should contain a courtesy nav section" do
-    visit "/"
     assert_have_selector "body > footer"
   end
   it "should contain a content area" do
-    visit "/"
     assert_have_selector "body > section"
   end
 
   describe "global navigation" do
     it "should link to the pages report" do
-      visit "/"
       assert_have_selector "body > header > nav"
       click_link "Pages"
     end
-    it "should link to the Sites report" do
-      visit "/"
+    it "should link to the sites report" do
       assert_have_selector "body > header > nav"
       click_link "Sites"
-
     end
     it "should link to the blog"
-    it "should link to the user's home page"
     it "should contain a contextual 'fun' widget" do
-      visit "/"
       assert_have_selector "body > header > a > img"
     end
 
   end
 
   describe "account section" do
-    describe "when logged in"do
-      it "should link to a tip page for a fan"
-      it "should link to a logout page when the fan is logged in"
+    describe "when logged in as a fan" do
+      before(:each) do
+        click_link "Log in or sign up"
+        fill_in "email", :with => "test@test.com"
+        fill_in "password", :with => "test"
+        choose "Yes, I have a password:"
+        click_button "Log in"
+      end
+
+      it "should have an account section" do
+        assert_have_selector "body > aside", :id => 'account'
+      end
+
+      it "should display the current user's name on the page" do
+        response_body.should contain("Logged in as: test@test.com")
+      end
+
+      it "should link to a logout action" do
+        click_link "Sign out"
+        response_body.should contain("Successfully logged out.")
+      end
+
+      it "should link to a tip page for a fan" do
+        click_link "Home"
+        response_body.should contain("Leave a Tip")
+      end
+
       it "should link to a fan home page"
-      it "should link to a publisher home page for a publisher"
-      it "should link to a administrators home page for an administrator"
+
     end
-    describe "when not logged in"do
-      it "should include a login or register widget when guest is unknown"
+
+    describe "when logged in as a publisher" do
+      before(:each) do
+        click_link "Log in or sign up"
+        fill_in "email", :with => "publisher@test.com"
+        fill_in "password", :with => "test"
+        choose "Yes, I have a password:"
+        click_button "Log in"
+      end
+
+      it "should link to a publisher home page for a publisher"
+    end
+
+
+    describe "when logged in as an administrator" do
+      before(:each) do
+        click_link "Log in or sign up"
+        fill_in "email", :with => "admin@test.com"
+        fill_in "password", :with => "test"
+        choose "Yes, I have a password:"
+        click_button "Log in"
+      end
+
+      it "should link to a administrators home page for an administrator" do
+        click_link "Admin Panel"
+        response_body.should contain("Admin home")
+      end
+
+    end
+
+    describe "when not logged in" do
+      it "should include a login or register widget when guest is unknown" do
+        click_link "Log in or sign up"
+      end
     end
   end
 
   describe "courtesy navigation" do
     it "should link to an Terms & Conditions  page" do
-      visit "/"
       assert_have_selector "body > footer > nav"
       click_link "Terms & Conditions"
     end
     it "should link to an Terms & Conditions  page" do
-      visit "/"
       assert_have_selector "body > footer > nav"
       click_link "Privacy"
     end
 
     it "should link to a subscribe page" do
-      visit "/"
       assert_have_selector "body > footer > nav"
       click_link "Subscribe"
     end
     it "should link to a contact page" do
-      visit "/"
       assert_have_selector "body > footer > nav"
       click_link "Contact"
     end
