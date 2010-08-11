2e37603f229eadc1e7ad4f67b74d6f149deab9be
Phil Mueller <phil@fasterlighterbetter.com>
2010-08-11 16:46:19 -0700

    CVV value is now required on Account model
    - Fixture updated with CVV (verification_value)
    - Tests updated accordingly.
    - Bug in account fixture fixed. Was previoulsy not loading proper card_type association.

--- app/models/account.rb
+++ app/models/account.rb
@@ -10,4 +10,6 @@ class Account < ActiveRecord::Base
   validates_presence_of :number
   validates_presence_of :expires_on
   validates_presence_of :card_type
+  validates_presence_of :verification_code
+  validates_numericality_of :verification_code, :integer_only => true
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -5,12 +5,13 @@ describe Account do
 
   before(:each) do
     @account = Account.new
-    @account.user            = users(:active)
-    @account.billing_address = addresses(:missoula)
-    @account.billing_name    = "CHOAD T HOCKSLOOGIE"
-    @account.expires_on      = Date.new(2013, 4, 1)
-    @account.card_type       = CardType.find_by_name('visa')
-    @account.number          = 4242_4242_4242_4242
+    @account.user               = users(:active)
+    @account.billing_address    = addresses(:missoula)
+    @account.billing_name       = "CHOAD T HOCKSLOOGIE"
+    @account.expires_on         = Date.new(2013, 4, 1)
+    @account.card_type          = CardType.find_by_name('visa')
+    @account.number             = 4242_4242_4242_4242
+    @account.verification_code  = 123
   end
 
   it "should save if all values are acceptable" do
@@ -42,14 +43,14 @@ describe Account do
     @account.save.should be_false
   end
 
-  it "should save without a card verification code" do
+  it "should have a verification code" do
     @account.verification_code = nil
-    @account.save.should be_true
+    @account.save.should be_false
   end
 
-  it "should save with a card verification code" do
-    @account.verification_code = 'CVV2'
-    @account.save.should be_true
+  it "should have an integer for a verification code" do
+    @account.verification_code = "foo"
+    @account.save.should be_false
   end
 
   it "should have a single field for the name from the card" do
