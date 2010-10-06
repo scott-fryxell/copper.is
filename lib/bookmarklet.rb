fbadd181a3893595ea1dcbffe617fc4021df4040
Forrest L Norvell <ogd@aoaioxxysz.net>
2010-10-06 16:59:56 -0700

    Wrote routine to URL encode a Javascript fragment into a bookmarklet.

---
+++ lib/bookmarklet.rb
@@ -0,0 +1,18 @@
+# Perl version by John Gruber, adapted into Ruby from
+# http://daringfireball.net/2007/03/javascript_bookmarklet_builder
+
+require 'uri'
+
+module Bookmarklet
+  def Bookmarklet.escape(unformatted_source)
+    source = unformatted_source
+    source.gsub!(/^\s*\/\/.+\n/m, '')  # Kill comments.
+    source.gsub!(/\t/m,   ' ')         # Tabs to spaces
+    source.gsub!(/ +/m,   ' ')         # Space runs to one space
+    source.gsub!(/^\s+/m, '')          # Kill line-leading whitespace
+    source.gsub!(/\s+$/m, '')          # Kill line-ending whitespace
+    source.gsub!(/\n/m,   '')          # Kill newlines
+
+    "javascript:" + URI.escape(source, /['" \x00-\x1f\x7f-\xff]/)
+  end
+end
