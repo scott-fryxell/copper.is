025cfe3ca2525177ef05c13f42efde66107ab6c4
Scott Fryxell <scott@communist.biz>
2010-12-03 22:12:29 -0800

    Using builder for dependency managment.

--- Gemfile
+++ Gemfile
@@ -1,12 +1,21 @@
-gem 'authlogic'
-gem 'ruby-openid'
-
-gem 'formtastic'
-gem "facebooker"
-
-gem 'activemerchant'
-
 source :gemcutter
+gem 'rails', '~> 2.3.9'
+gem "sqlite3-ruby", :require => "sqlite3"
+gem 'authlogic'
 gem 'declarative_authorization'
+gem "facebooker"
+gem 'ruby-openid'
+gem "spork"
 
-gem 'rails', '2.3.9', :require => nil
+
+
+group :development do
+  gem 'wirble'
+  gem 'rails-footnotes'
+end
+
+group :test do
+  gem "rspec", '~> 1.3.1'
+  gem 'rspec-rails', '~> 1.3.3'
+  gem 'webrat'
+end
--- config/environment.rb
+++ config/environment.rb
@@ -19,11 +19,11 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "authlogic"
-  config.gem "activemerchant", :lib => "active_merchant"
-  config.gem "declarative_authorization", :source => "http://www.gemcutter.org"
-  config.gem 'formtastic'
-  config.gem "facebooker"
+  # config.gem "authlogic"
+  # config.gem "activemerchant", :lib => "active_merchant"
+  # config.gem "declarative_authorization", :source => "http://www.gemcutter.org"
+  # config.gem 'formtastic'
+  # config.gem "facebooker"
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
