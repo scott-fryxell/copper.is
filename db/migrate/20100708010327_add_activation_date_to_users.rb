325b18af6fc8fd1daaa191b2fb7127bc607b8958
Scott Fryxell <scott@communist.biz>
2010-12-07 13:23:03 -0800

    Removing mirgrations for user managment.

--- db/migrate/20100708010327_add_activation_date_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddActivationDateToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :activation_date, :datetime
-  end
-
-  def self.down
-    remove_column :users, :activation_date
-  end
-end
--- db/migrate/20101105071323_add_new_email_token_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddNewEmailTokenToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :new_email_token, :string
-  end
-
-  def self.down
-    remove_column :users, :new_email_token
-  end
-end
