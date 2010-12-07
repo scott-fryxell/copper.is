168329b4eb38519d909775e688ecce5d0335d033
Scott Fryxell <scott@communist.biz>
2010-12-07 13:22:21 -0800

    Removing facebook connect

--- db/migrate/20101111003811_add_facebook_session_key_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddFacebookSessionKeyToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :facebook_session_key, :string
-  end
-  def self.down
-    remove_column :users, :facebook_session_key
-  end
-end
