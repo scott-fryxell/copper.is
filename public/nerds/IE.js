92211c59d2dd7242eee20cc352822951209d05bb
Scott Fryxell <scott.fryxell@styleowner.com>
2011-06-15 21:30:18 -0700

    Breathing new life into things.
    Put the logo under source control.
    
    Pruned unused files. 
    
    Added a favicon.ico.
    
    Removed code I did to get a job.

--- public/nerds/IE.js
+++
@@ -1,5 +0,0 @@
-$('body > section > details > summary').live("click.display", function (event){
-  $('body > section > details[open=closed]').hide().show()
-  $(this).parent().hide();
-  $(this).parent().show();
-});
