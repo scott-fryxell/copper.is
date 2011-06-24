f56571c90dda8af62dbdadb6d6e9ff23185b1229
Scott Fryxell <scott.fryxell@styleowner.com>
2011-06-24 08:19:33 -0700

    refactored javascript for readability.
    updated gems.
    
    bookmarklet fully disapears after tip.

--- public/agent/behavior.js
+++ public/agent/behavior.js
@@ -4,69 +4,61 @@ $(document).ready(function() {
   });
 });
 
-$(document).bind("ajaxComplete", function (event, xhr, options) {
-  $(document).trigger(new String(xhr.status), xhr, options);
-});
+$(document).bind({
+  "ajaxComplete": function (event, xhr, options) {
+    $(document).trigger(new String(xhr.status), xhr, options);
+  },
+  "200": function (event, xhr, options){
+    var trigger = $("<div />").append(xhr.responseText).find("meta[name=event_trigger]").attr("content");
+    if(trigger){
+      $(document).trigger(trigger, xhr, options);
+    }
+  }, 
+  "notify": function (event, xhr, options) {
+    $("section.notify").append(xhr.responseText);
+    $("body").addClass("open");
 
-$(document).bind("200", function (event, xhr, options){
-  var trigger = $("<div />").append(xhr.responseText).find("meta[name=event_trigger]").attr("content");
-  if(trigger){
-    $(document).trigger(trigger, xhr, options);
-  }
-});
+    $("section.notify").fadeIn(800).delay(2000).fadeOut(800, function(){
+      window.parent.postMessage("notify_complete",  "*");
+      $("body").removeClass("open");
+    });
+  },
+  "open": function (event, url) {
+    window.open(url);
+  },
+  "workflow_start": function (event, xhr) {
+    $("body").addClass("open");
+    $("section.workflow > header > .close").click(function(){
+      $(document).trigger("workflow_end");
+    });
 
-$(document).bind("notify", function (event, xhr, options) {
-  $("section.notify").append(xhr.responseText);
-  $("body").addClass("open");
-
-  $("section.notify").fadeIn(800).delay(2000).fadeOut(800, function(){
-    window.parent.postMessage("notify_complete",  "*");
+    $("section.workflow").slideToggle(1200);
+  },
+  "workflow_end": function (event) {
     $("body").removeClass("open");
-  });
-
-});
-
-$(document).bind("open", function (event, url) {
-  window.open(url);
-});
-
-$(document).bind("workflow_start", function (event, xhr) {
-  $("body").addClass("open");
-  $("section.workflow > header > .close").click(function(){
-    $(document).trigger("workflow_end");
-  });
-
-  $("section.workflow").slideToggle(1200);
-});
-
-$(document).bind("workflow_end", function (event) {
-  $("body").removeClass("open");
-  $("body").focus();
-  $("section.workflow > header > .close").unbind();
-  $("section.workflow").slideToggle(1000);
-});
-
-$(document).bind("alert_start", function (event, xhr) {
-  $("section.alert ol").append("<li>" +  Message[xhr.status] + "</li>");
-  $("section.alert").fadeIn(500);
-});
-
-$(document).bind("alert_end", function (event, xhr) {
-  $("section.alert ol").empty();
-  $("section.alert").fadeOut("slow");
-});
-
-$(document).keyup(function(event) {
-
-  /*
-    TODO needs to be aware of what mode app is in (workflow, open, notify, alert)
-  */
-  if (event.keyCode == 27 ){
-    if($("body.open").length == 1 )
-      $(document).trigger("notify");
-    else
-      $(document).trigger("notify");
+    $("body").focus();
+    $("section.workflow > header > .close").unbind();
+    $("section.workflow").slideToggle(1000);
+  },
+  "alert_start": function (event, xhr) {
+    $("section.alert ol").append("<li>" +  Message[xhr.status] + "</li>");
+    $("section.alert").fadeIn(500);
+  },
+  "alert_end": function (event, xhr) {
+    $("section.alert ol").empty();
+    $("section.alert").fadeOut("slow");
+  },
+  "keyup": function(event) {
+    /*
+      TODO needs to be aware of what mode app
+      is in (workflow, open, notify, alert)
+    */
+    if (event.keyCode == 27 ){
+      if($("body.open").length == 1 )
+        $(document).trigger("notify");
+      else
+        $(document).trigger("notify");
+    }
   }
-
 });
 
--- public/bookmarklet/behavior.js
+++ public/bookmarklet/behavior.js
@@ -1,86 +1,75 @@
-var FLB = {
-  tip: {}
-};
-
 $(document).ready(function() {
    $(document).trigger("tip_determine");
    $(document).trigger("tip_token_get");
    $(document).trigger("tip_submit");
 });
-
-$(document).bind("tip_determine", function (event, xhr, options){
-
-  var location = new String(window.location);
-
-  //assumes loaded in an iframe with  the url to be tipped appended
-  if(location.split("#/")[1] && location.split("&title=")[1] ){
-    FLB.tip = {
-      uri: location.split("#/")[1].split("&title=")[0],
-      title: location.split("&title=")[1],
-    };
-  }
-});
-
-$(document).bind("tip_token_get", function (event, xhr, options){
-  // get authenticity token
-  $.ajax({
-    url: "/tips/new.js",
-    async:false
-  });
-});
-
-$(document).bind("tip_submit", function (event, xhr, options){
-  if(FLB.tip.authenticity_token != null){
+$(document).bind({
+  "tip_determine": function (event, xhr, options){
+    var location = new String(window.location);
+    //assumes loaded in an iframe with  the url to be tipped appended
+    if(location.split("#/")[1] && location.split("&title=")[1] ){
+      FLB.tip = {
+        uri: location.split("#/")[1].split("&title=")[0],
+        title: location.split("&title=")[1],
+      };
+    }
+  },
+  "tip_token_get": function (event, xhr, options){
+    // get authenticity token
     $.ajax({
-      url:"/tips",
-      data: FLB.tip,
-      type: "POST"
+      url: "/tips/new.js",
+      async:false
     });
+  },
+  "tip_submit": function (event, xhr, options){
+    if(FLB.tip.authenticity_token != null){
+      $.ajax({
+        url:"/tips",
+        data: FLB.tip,
+        type: "POST"
+      });
+    }
+  },
+  "tip_success": function (event, xhr, options){
+    $(document).trigger("notify", xhr, options);
+  },
+  "tip_error": function (event, xhr, options){
+    /*
+      TODO implement or remove.
+    */
+    $(document).trigger("alert_start", xhr);
+    console.debug("there was a tip error", xhr, event);
+  },
+  "login_submit": function (event, options){
+    $.post("/authenticate", $("section.workflow form").serialize());
+    $(document).trigger("workflow_end");
+  },
+  "login_get": function (event){
+    $("section.workflow ").load('/signin');
+  },
+  "login_display": function (event){
+    $("section.workflow > form").submit(function (event){
+      event.preventDefault();
+      $(document).trigger("login_submit");
+      return false;
+    });
+
+    $("section.workflow > header > .click").click(function (){
+      $(document).trigger("end_workflow");
+      return false;
+    });
+
+    $(document).trigger("workflow_start");
+    $("input[id=email]").delay(1200).focus();
+  },
+  "login_success": function (event){
+    $(document).trigger("tip_token_get");
+    $(document).trigger("tip_submit");
+  },
+  "401": function (event, response, options){
+    $(document).trigger("login_get");
   }
 });
-
-$(document).bind("tip_success", function (event, xhr, options){
-  $(document).trigger("notify", xhr, options);
-});
-
-$(document).bind("tip_error", function (event, xhr, options){
-  /*
-    TODO implement or remove.
-  */
-  $(document).trigger("alert_start", xhr);
-  console.debug("there was a tip error", xhr, event);
-});
-
-$(document).bind("login_submit", function (event, options){
-  $.post("/authenticate", $("section.workflow form").serialize());
-  $(document).trigger("workflow_end");
-});
-
-$(document).bind("login_get", function (event){
-  $("section.workflow ").load('/signin');
-});
-
-$(document).bind("login_display", function (event){
-  $("section.workflow > form").submit(function (event){
-    event.preventDefault();
-    $(document).trigger("login_submit");
-    return false;
-  });
-
-  $("section.workflow > header > .click").click(function (){
-    $(document).trigger("end_workflow");
-    return false;
-  });
-
-  $(document).trigger("workflow_start");
-  $("input[id=email]").delay(1200).focus();
-});
-
-$(document).bind("login_success", function (event){
-  $(document).trigger("tip_token_get");
-  $(document).trigger("tip_submit");
-});
-
-$(document).bind("401", function (event, response, options){
-  $(document).trigger("login_get");
-});
+var FLB = {
+  tip: {}
+};
