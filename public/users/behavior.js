4f3793ba1309d277534143280a3d651adf4cd269
Scott Fryxell <scott.fryxell@styleowner.com>
2011-07-05 11:57:14 -0700

    Fix for details tag messing up chrome.

--- public/users/behavior.js
+++ public/users/behavior.js
@@ -6,35 +6,34 @@ $(document).ready(function () {
 
   $.get('/users/current.json', function(data) {
     var tip_rate = data.user.tip_preference_in_cents
-    var option = $('details#tip_rate > form > fieldset > select > option[value=' + tip_rate +']')
+    var option = $('#tip_rate > form > fieldset > select > option[value=' + tip_rate +']')
     option.attr('selected', true)
 
-    $('details#tip_rate > form').attr('action', '/users/' + data.user.id)
+    $('#tip_rate > form').attr('action', '/users/' + data.user.id)
   });
 
-  $('details > summary').click(function (event){
-    if ($(this).parent('details').attr('open') == 'open')
-      $(this).parent('details').attr('open', 'close')
+  $('div > summary').click(function (event){
+    if ($(this).parent('div').attr('open') == 'open')
+      $(this).parent('div').attr('open', 'close')
     else
-      $(this).parent('details').attr('open', 'open')
+      $(this).parent('div').attr('open', 'open')
   });
 
-  $('details#bookmarklet > p > a ').bind('dragend', function (event) {
-    $('details#bookmarklet').removeClass('required');
-    $('details#bookmarklet').addClass('completed');
-    $('details#bookmarklet > summary').click();
+  $('#bookmarklet > p > a ').bind('dragend', function (event) {
+    $('#bookmarklet').removeClass('required');
+    $('#bookmarklet').addClass('completed');
+    $('#bookmarklet > summary').click();
   })
 
-  $('details#tip_rate > form > fieldset > select').change(function (event) {
-    $('details#tip_rate').removeClass('required');
-    $('details#tip_rate').addClass('completed');
-    $('details#tip_rate > summary').click();
-
+  $('#tip_rate > form > fieldset > select').change(function (event) {
+    $('#tip_rate').removeClass('required');
+    $('#tip_rate').addClass('completed');
+    $('#tip_rate > summary').click();
 
     $.ajax({
       type: 'POST',
-      url: $('details#tip_rate > form').attr('action'),
-      data: $('details#tip_rate > form').serialize(),
+      url: $('#tip_rate > form').attr('action'),
+      data: $('#tip_rate > form').serialize(),
     });
 
   });
