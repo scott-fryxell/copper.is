de693d4b89cf97d5a190311df6f9996b6c6ac689
scott fryxell <sfryxell@me.com>
2011-09-11 17:40:08 -0700

    Removing tests we're no longer using.

--- spec/javascripts/spec_helper.js
+++
@@ -1,7 +0,0 @@
-// Use this file to require common dependencies or to setup useful test functions.
-
-function clean_up(){
-  $("section.notify").empty();
-  $("section.workflow").empty();
-  $("section.alert > ol").empty();
-}
