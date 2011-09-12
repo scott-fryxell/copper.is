60a8c52139f136082557a1309923b4329f171b93
scott fryxell <sfryxell@me.com>
2011-09-11 17:43:06 -0700

    moving secret key to an environment variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DirtyWhiteCouch::Application.config.secret_token = '31732fdf468d0696c48f3bcc7154ca33795742214730ec33dc171216cdeb0e1c90aff63b994729a4ace54bbe281d802b163bd197e42ea5bdbb9b5dc0675f46a2'
+DirtyWhiteCouch::Application.config.secret_token = ENV['WEAVE_SECRET_KEY']
