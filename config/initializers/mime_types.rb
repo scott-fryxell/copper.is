69c0518605fae2a701b916985fbd8f0d65bfca14
scott fryxell <sfryxell@me.com>
2011-09-19 15:41:22 -0700

    mime types for chrome.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -4,3 +4,4 @@
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
 Mime::Type.register "application/octet-stream", :safariextz
+Mime::Type.register "application/octet-stream", :crx
