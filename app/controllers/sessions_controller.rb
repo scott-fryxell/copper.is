e108ff1cbed40b1c9d6a5403d96fd08b5ff1f02b
scott fryxell <sfryxell@me.com>
2011-09-19 16:18:06 -0700

    setting domain for cookie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
-    cookies[:user_id] = {:value => user.id, :expires => 90.days.from_now}
+    cookies[:user_id] = {:value => user.id, :expires => 90.days.from_now, :domain => '.dirtywhitecouch.com'}
     redirect_to tips_url, :notice => "Signed In!"
   end
 
