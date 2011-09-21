ab2fffd3c325ce39afaec5445eac87ebb3934d40
scott fryxell <sfryxell@me.com>
2011-09-21 01:39:23 -0700

    extension for firefox.
    updates to iou image for safari and chrome extension
    
    update bookmarklet to reflect new file structure.

---
+++ user-agents/firefox/chrome/button.js
@@ -0,0 +1,7 @@
+CustomButton = {
+  1: function () {
+    var s = content.document.createElement('script');
+    s.src = 'http://dirtywhitecouch.com/tips/embed_iframe.js';
+    content.document.body.appendChild(s);
+  },
+}
