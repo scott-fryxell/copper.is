4159d7cf3884a0886b7e64cfb4440c87af4333c3
scott fryxell <sfryxell@me.com>
2011-09-22 22:12:50 -0700

    Correctly embed script tag.

--- user-agents/firefox/chrome/button.js
+++ user-agents/firefox/chrome/button.js
@@ -1,7 +1,7 @@
 CustomButton = {
   1: function () {
-    var s = content.document.createElement('script');
+    var s = window.content.document.createElement('script');
     s.src = 'http://dirtywhitecouch.com/tips/embed_iframe.js';
-    content.document.head.appendChild(s);
+    window.content.document.body.appendChild(s);
   },
 }
