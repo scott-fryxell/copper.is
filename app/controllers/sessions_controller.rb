fb800b72d3da6d3c771fbb5378f05ea8c421ebad
scott fryxell <sfryxell@me.com>
2011-09-25 23:25:09 -0700

    setting the cookie for the whole domain.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,13 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     # session[:user_id] = user.id
-    cookies[:user_id] = {:value => user.id, :expires => 90.days.from_now}
+    cookies[:user_id] = {:value => user.id, :expires => 90.days.from_now, :domain => '.dirtywhitecouch.com'}
     redirect_to tips_url, :notice => "Signed In!"
   end
 
   def destroy
     # session[:user_id] = nil
-    cookies[:user_id] = {:value => nil, :expires => 90.days.ago}
+    cookies[:user_id] = {:value => nil, :expires => 90.days.ago, :domain => '.dirtywhitecouch.com'}
     redirect_to root_url, :notice => "Signed out!"
   end  
   
