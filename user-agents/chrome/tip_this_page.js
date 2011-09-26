ad54bf63616c711fa2f794968a1f89930f5b7425
scott fryxell <sfryxell@me.com>
2011-09-26 12:04:02 -0700

    Updating chrome. works with flash now.

--- user-agents/chrome/tip_this_page.js
+++
@@ -1,6 +0,0 @@
-chrome.extension.onRequest.addListener( function(request, sender, sendResponse) {
-  var s = document.createElement('script');
-  s.src = 'http://dirtywhitecouch.com/tips/embed_iframe.js';
-  document.body.appendChild(s);
-});
-
