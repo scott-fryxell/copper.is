9beec3aa3ed699ba5ea2b2a468fb7a18b6f06cd1
scott fryxell <sfryxell@me.com>
2011-09-26 21:02:11 -0700

    post this message as soon as the script is loaded.

--- public/users/behavior.js
+++ public/users/behavior.js
@@ -1,3 +1,4 @@
+window.parent.postMessage("reset_frame",  "*");
 $(document).ready(function () {
   $.get('/users/current.json', function(data) {
     var tip_rate = data.user.tip_preference_in_cents
@@ -33,5 +34,5 @@ $(document).ready(function () {
     });
 
   });
-  window.parent.postMessage("reset_frame",  "*");
+
 });
