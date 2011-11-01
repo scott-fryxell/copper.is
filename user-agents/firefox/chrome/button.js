fa811fb59cb8c9ba9907a531efd655b72bdfb861
Scott Fryxell <scott@dirtywhitecouch.com>
2011-11-01 11:20:27 -0700

    Moving extensions to there own repo

--- user-agents/firefox/chrome/button.js
+++
@@ -1,7 +0,0 @@
-CustomButton = {
-  1: function () {
-    var s = window.content.document.createElement('script');
-    s.src = 'http://dirtywhitecouch.com/tips/embed_iframe.js';
-    window.content.document.body.appendChild(s);
-  },
-}
--- user-agents/safari.safariextension/tip_this_page.js
+++
@@ -1,9 +0,0 @@
-safari.self.addEventListener("message", function (msgEvent){
-  if (msgEvent.name === "tip_this_page"){    
-    if (window.top === window) {
-      var s = document.createElement('script');
-      s.src = 'http://dirtywhitecouch.com/tips/embed_iframe.js';
-      document.body.appendChild(s);
-    }
-  }
-}, false);
