a4696f7cb15f69fd29b69f3452d8bfde096d9ff6
Scott Fryxell <scott@dirtywhitecouch.com>
2011-12-05 15:14:24 -0800

    Config for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,10 +2,10 @@ DirtyWhiteCouch::Application.configure do
   config.cache_classes = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
-  config.serve_static_assets = false
+  config.action_dispatch.x_sendfile_header = "X-Accel-Redirect"
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
+  config.serve_static_assets = true
   config.assets.compress = true
   config.assets.js_compressor  = :uglifier
   config.assets.compile = true
