c2ef26e87cfca32042eed04bd5c753d6cbc81732
Scott Fryxell <scott@dirtywhitecouch.com>
2011-12-14 13:41:24 -0800

    testing

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,7 +4,7 @@ DirtyWhiteCouch::Application.configure do
   config.action_dispatch.x_sendfile_header = nil # For Heroku
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
-  config.serve_static_assets = true
+  config.serve_static_assets = false
   config.assets.compress = true
   config.assets.js_compressor  = :uglifier
   config.assets.compile = true
@@ -13,7 +13,6 @@ DirtyWhiteCouch::Application.configure do
 
   config.action_controller.perform_caching = true
   config.cache_store = :dalli_store
-  config.static_cache_control = 'public/assets, max-age=86400'
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
