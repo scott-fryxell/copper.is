958a9a623a7cd0fb30bc0d2d63b81c39a6bbb025
Scott Fryxell <scott@dirtywhitecouch.com>
2011-12-15 12:55:40 -0800

    Updating to newest config for development staging and production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,16 +1,22 @@
 DirtyWhiteCouch::Application.configure do
   config.cache_classes = true
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.consider_all_requests_local       = true
   config.action_dispatch.x_sendfile_header = "X-Accel-Redirect"
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
-  config.serve_static_assets = true
+
   config.assets.compress = true
   config.assets.js_compressor  = :uglifier
+  # config.assets.css_compressor = :scss
   config.assets.compile = true
   config.assets.debug = false
   config.assets.digest = true
+
+  config.action_controller.perform_caching = true
+  config.cache_store = :dalli_store
+
+  config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=315360000"
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
@@ -21,5 +27,4 @@ DirtyWhiteCouch::Application.configure do
     :password             => DirtyWhiteCouch::Application.config.email_password,
     :authentication       => 'plain',
     :enable_starttls_auto => true  }
-
 end
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,11 +1,9 @@
 DirtyWhiteCouch::Application.configure do
   config.cache_classes = true
-  config.consider_all_requests_local       = false
-  config.action_dispatch.x_sendfile_header = nil # For Heroku
+  config.consider_all_requests_local       = true
+  config.action_dispatch.x_sendfile_header = "X-Accel-Redirect"
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
-
-  # config.action_controller.asset_host = "xxxxxxxxxxxxx.cloudfront.net"
 
   config.assets.compress = true
   config.assets.js_compressor  = :uglifier
@@ -15,7 +13,7 @@ DirtyWhiteCouch::Application.configure do
   config.assets.digest = true
 
   config.action_controller.perform_caching = true
-  # config.cache_store = :dalli_store
+  config.cache_store = :dalli_store
 
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=315360000"
