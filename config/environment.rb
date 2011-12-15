3cd688886acb59ad85e0faa682c7a24a57ccb83a
Scott Fryxell <scott@dirtywhitecouch.com>
2011-12-14 16:07:50 -0800

    Clean out some unused documentation.

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,3 @@
-# Be sure to restart your server when you modify this file
-
-# Specifies gem version of Rails to use when vendor/rails is not present
-# RAILS_GEM_VERSION = '2.3.9' unless defined? RAILS_GEM_VERSION
-
-# Bootstrap the Rails environment, frameworks, and default configuration
-# Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
