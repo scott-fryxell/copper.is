e48f4eb6bdb58d1afbc6382b62762ab4dfe74a00
Scott Fryxell <scott@dirtywhitecouch.com>
2011-12-14 16:08:17 -0800

    Asset caching. (this will most likely fail).

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,15 +4,18 @@ DirtyWhiteCouch::Application.configure do
   config.action_dispatch.x_sendfile_header = nil # For Heroku
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
-  config.serve_static_assets = false
+  config.serve_static_assets = true
   config.assets.compress = true
   config.assets.js_compressor  = :uglifier
+  config.assets.css_compressor = :yui
+
   config.assets.compile = true
   config.assets.debug = false
   config.assets.digest = true
 
   config.action_controller.perform_caching = true
   config.cache_store = :dalli_store
+  config.static_cache_control = 'public/assets, max-age=315360000'
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
