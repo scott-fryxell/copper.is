085e52a4045c3283c4cd087077ee7ab5053791e9
Scott Fryxell <scott@dirtywhitecouch.com>
2012-01-11 16:11:08 -0800

    Remove the bcc.

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,6 +4,6 @@ class OrderMailer < ActionMailer::Base
 
   def reciept(order)
     @order = order
-    mail(:to => order.fan.email, :subject => "DWC has processed your tips " + order.fan.name, :bcc => "orders@dirtywhitecouch.com")
+    mail(:to => order.fan.email, :subject => "DWC has processed your tips " + order.fan.name)
   end
 end
