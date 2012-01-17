f3c88f8f4b0eaef51e0fdf8edc971de3798ec19f
Scott Fryxell <scott@dirtywhitecouch.com>
2012-01-16 19:10:54 -0800

    Fix check for time to pay.

--- app/models/tip_order.rb
+++ app/models/tip_order.rb
@@ -10,8 +10,6 @@ class TipOrder < ActiveRecord::Base
   validates_uniqueness_of :fan_id, :scope => :is_active, :if => :is_active
 
   def time_to_pay?
-    return true
-    
     if ( self.tiped_enough_to_pay? && self.old_enough_to_pay? && !self.fan.automatic_rebill )
       return true
     else
