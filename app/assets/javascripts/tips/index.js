35ef44bb7ffb62735bf24e712a831ff602167a09
Scott Fryxell <scott@dirtywhitecouch.com>
2012-03-12 15:54:31 -0700

    Populate the tip form after the current user is retrieved.

--- app/assets/javascripts/tips/index.js
+++ app/assets/javascripts/tips/index.js
@@ -1,22 +1,20 @@
 //= require shared/current_user
 //= require shared/tip_rate
-
 window.parent.postMessage("reset_frame",  "*");
-
 $(document).ready(function() {
   if(window.location.hash == ""){
     $(document).trigger('#active_tips');
   }
-  $('#new_tip').attr('action', '/users/' + current_user.id + '/tips');
   $('td > form').bind('submit', function(){
     $(this).parent().parent().addClass('destroyed');
   });
 });
-
 $(document).bind("#all_tips", function(event){
   $('#active_tips').css('opacity', '.5');
 });
-
 $(document).bind("#active_tips", function(event){
   $('#all_tips').css('opacity', '.5');
+});
+$(document).bind("get:current_user", function(event){
+  $('#new_tip').attr('action', '/users/' + current_user.id + '/tips');
 });
