88b7fa2520da0412bdf0852230aba8fa4e3c78fd
Scott Fryxell <scott@dirtywhitecouch.com>
2012-03-12 17:59:12 -0700

    one more swipe at figuring out the cache.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,30 +1,6 @@
 class HomeController < ApplicationController
   respond_to :html
 
-  caches_action :index, :blog, :terms, :privacy, :button, :authors
-
-  def index
-    render :action => 'index'
-  end
-
-  def blog
-    render :action => 'blog'
-  end
-
-  def terms
-    render :action => 'terms'
-  end
-
-  def privacy
-    render :action => 'privacy'
-  end
-
-  def button
-    render :action => 'button'
-  end
-
-  def authors
-    render :action => 'authors'
-  end
+  caches_page [:index, :blog, :terms, :privacy, :button, :authors]
 
 end
