93adaea98209365dd2f370e16c62e7ad2c2f7d1d
Scott Fryxell <scott@dirtywhitecouch.com>
2012-03-12 18:27:13 -0700

    bunch of caches actions.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   respond_to :html
-
-  caches_page [:index, :blog, :terms, :privacy, :button, :authors]
-
+  caches_action :index
+  caches_action :blog
+  caches_action :terms
+  caches_action :privacy
+  caches_action :button
+  caches_action :authors
+  caches_action :about
+  caches_action :contact
 end
