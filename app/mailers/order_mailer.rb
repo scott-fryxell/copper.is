d4da28461afab87031bea94a62d96c2b2f261139
Michael Garriss <mgarriss@gmail.com>
2012-04-03 15:28:24 -0700

    new mailer template, need copy

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,8 +1,18 @@
 class OrderMailer < ActionMailer::Base
   default :from => "orders@copper.is"
   helper :Layout
-
+  
   def reciept(order)
+    @order = order
+    mail(:to => order.fan.email, :subject => "Copper has processed your tips " + order.fan.name)
+  end
+  
+  def card_declinded(order)
+    @order = order
+    mail(:to => order.fan.email, :subject => "Copper has processed your tips " + order.fan.name)
+  end
+  
+  def processing_error(order)
     @order = order
     mail(:to => order.fan.email, :subject => "Copper has processed your tips " + order.fan.name)
   end
