e9a9a456486b4c41ca8ef2fd875a0a27a1584a09
Michael Garriss <mgarriss@gmail.com>
2012-04-04 12:02:42 -0700

    spec for making sure TipOrderChargeJob.perform doesn't explode

---
+++ spec/functionals/tip_order_charge_job_spec.rb
@@ -0,0 +1,28 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe TipOrderChargeJob do
+  fixtures :roles, :users, :roles_users, :tip_orders, :tips
+  
+  before do
+  #   Resque.inline = true
+  #   @worker = Resque::Worker.new(:high)
+    @tip_order = TipOrder.create fan:users(:a_developer)
+    @tip_order.valid?.should be_true
+  end
+  
+  # it "queue onto the Resque high priority queue" do
+  #   Resque.enqueue(TipOrderChargeJob, @tip_order.id)
+  #   @worker.failed.should == 0
+  #   @worker.processed.should == 0
+  #   @worker.work do |job|
+  #     @worker.failed.should == 0
+  #     @worker.processed.should == 1
+  #   end
+  # end
+  
+  it 'has a working .perform' do
+    @tip_order.stub(:charge)
+    TipOrderChargeJob.perform(@tip_order.id)
+  end
+end
+
