a8b37c4a7f3ef36392b6d2b7ebae86c35e31f5b9
Scott Fryxell <scott@copper.is>
2012-04-05 15:08:41 -0700

    Set up authorization rules so that authors can see their royalties.

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -3,18 +3,12 @@ authorization do
   role :administrator do
   end
 
-  role :developer do
-
-  end
-
-  role :publisher do
-  end
-
   role :patron do
     has_permission_on [:sessions], :to => [:destroy]
 
     has_permission_on [:users], :to => [:new, :create, :edit, :update, :show, :pay, :identities, :author] do
       has_permission_on [:tips], :to => [:index, :create, :edit, :update, :destroy, :new, :embed_iframe, :agent]
+      has_permission_on [:royalties], :to => [:index]
     end
   end
 
