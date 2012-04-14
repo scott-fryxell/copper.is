5b859d2e7ddb012927ffd219bb846e4b1c3a0bbc
Scott Fryxell <scott@copper.is>
2012-04-14 10:50:38 -0700

    updated royalty_checks_controller_spec with urls

--- spec/controllers/royalty_checks_controller_spec.rb
+++ spec/controllers/royalty_checks_controller_spec.rb
@@ -6,25 +6,51 @@ describe RoyaltyChecksController do
       unauthenticate
     end
     describe 'index' do
-      it '302'
+      describe '/royalty_checks' do
+        it '302'
+      end
     end
     describe 'new' do
-      it '302'
+      describe '/royalty_checks/new' do
+        it '302'
+      end
     end
     describe 'create' do
-      it '302'
+      describe 'POST /royalty_checks' do
+        it '302'
+      end
     end
     describe 'show' do
-      it '302'
+      describe '/royalty_checks/:id' do
+        it '302'
+      end
+      describe '/royalty_checks/current' do
+        it '302'
+      end
     end
-    describe 'eddescribe' do 
-      it '302'
+    describe 'edit' do
+      describe '/royalty_checks/:id/edit' do
+        it '302'
+      end
+      describe '/royalty_checks/current/edit' do
+        it '302'
+      end
     end
     describe 'update' do
-      it '302'
+      describe 'PUT /royalty_checks/:id' do
+        it '302'
+      end
+      describe 'PUT /royalty_checks/current' do
+        it '302'
+      end
     end
     describe 'destroy' do
-      it '302'
+      describe 'DELETE /royalty_checks/:id' do
+        it '302'
+      end
+      describe 'DELETE /royalty_checks/current' do
+        it '302'
+      end
     end
   end
   
@@ -33,28 +59,67 @@ describe RoyaltyChecksController do
       authenticate_as_patron
     end
     describe 'index' do
-      it 'renders all royalty_checks for current user, :earned, :paid and :cashed'
-      it 'with ?state=earned, renders just the current earned royalty_check for the current user'
-      it 'with ?state=paid, renders all paid royalty_checks for current user'
-      it 'with ?state=cashed, renders all cashed royalty_checks for current user'
+      describe '/royalty_checks' do
+        it 'renders all royalty_checks for current user: earned, paid and cashed'
+      end
+      describe '/royalty_checks?s=earned' do
+        it 'renders just the current royalty_check for the current user'
+      end
+      describe '/royalty_checks?s=paid' do
+        it 'renders all paid royalty_checks for current user'
+      end
+      describe '/royalty_checks?s=cashed' do
+        it 'renders all declined royalty_checks for current user'
+      end
     end
     describe 'new' do
-      it '404' 
+      describe '/royalty_checks/new' do
+        it '403'
+      end
     end
     describe 'create' do
-      it '404'
+      describe 'POST /royalty_checks' do
+        it '403'
+      end
     end
     describe 'show' do
-      it 'renders a given tip_order if owned by current user'
+      describe '/royalty_checks/current' do
+        it 'renders the current earned royalty_check of current user'
+      end
+      describe '/royalty_checks/:id' do
+        it 'renders the given royalty_check when owned by current user'
+        it '302 to /royalty_checks/current if it\'s the current earned royalty_check'
+        it '403 if royalty_check is not owned by current user'
+      end
     end
     describe 'edit' do
-      it '403'
+      describe '/royalty_checks/current/edit' do
+        it '403'
+      end
+      describe '/royalty_checks/:id/edit' do
+        it '403'
+      end
     end
     describe 'update' do
-      it 'prepares! the current tip_order for the current user'
+      describe 'PUT /royalty_checks/current' do
+        it 'prepares! the current royalty_check for the current user'
+        it "doesn't allow any column updates to the royalty_check"
+      end
+      describe 'PUT /tips_orders/:id' do
+        it 'prepares! the current royalty_check for the current user'
+        it '302 to /royalty_checks/current if it\'s the current earned royalty_check'
+        it '403 if not owned by current user'
+      end
     end
     describe 'destroy' do
-      it '404'
+      describe 'DELETE /royalty_checks/current' do
+        it 'destroys all contained tips, destroys current royalty_check and creates a new royalty_check'
+      end
+      describe 'DELETE /royalty_checks/:id' do
+        it 'if :id is the current royalty_check it destroys all contained tips, destroys current royalty_check and creates a new royalty_check'
+        it '403 if royalty_check is not current and owned by current user'
+        it '403 it royalty_check is not current and not owned by current user'
+      end
     end
   end
   
