bfa2bbbe24f2986cf135c5db43e66b50c22d8767
Scott Fryxell <scott@copper.is>
2012-04-19 15:27:08 -0700

    remove period from notice when removing an identity

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -7,6 +7,6 @@ class IdentitiesController < ApplicationController
   def destroy 
     identity = Identity.find(params[:id])
     identity.destroy
-    redirect_to user_identities_path(current_user), notice: "Removed that Identity."
+    redirect_to user_identities_path(current_user), notice: "Removed that Identity"
   end
 end
