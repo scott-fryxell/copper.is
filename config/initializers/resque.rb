c72b090022ce84d709d2e3db219d32ebe3f00f09
Scott Fryxell <scott@copper.is>
2012-04-30 11:48:30 -0700

    push rescue to staging

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,7 +1,5 @@
 Dir[File.join(Rails.root, 'app', 'jobs', '*.rb')].each do |file| 
   require file
 end
-
-config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
-
-Resque.redis = Redis.new host: config['host'], port: config['port']
+# config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
+# Resque.redis = Redis.new host: config['host'], port: config['port']
