35cc5330acc8a140ecb3ac670e26bf97e1d2666e
Scott Fryxell <scott@copper.is>
2012-04-30 10:27:39 -0700

    a guest author can authorize an identity to receive royalties

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -1,22 +1,21 @@
 class IdentitiesController < ApplicationController
   filter_access_to :all
-  
-  def index
 
+  def index
   end
-  
+
   def show
     @identity = Identity.find(params[:id])
   end
-  
-  def destroy 
+
+  def destroy
     identity = Identity.find(params[:id])
     identity.destroy
     redirect_to user_identities_path(current_user), notice: "Removed that Identity"
   end
-  
+
   def wanted
     @wanted = Identity.wanted.all
   end
-  
+
 end
