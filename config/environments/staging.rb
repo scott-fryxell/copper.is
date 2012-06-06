3fa7d89a4a917850da72f1b5c8fc4b851a90a795
Scott Fryxell <scott@copper.is>
2012-06-05 20:25:30 -0700

    remove asset crap to see if that's what's borking staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -13,7 +13,7 @@ Copper::Application.configure do
   config.assets.compile = true
   config.assets.debug = false
   config.assets.digest = true
-  config.action_controller.asset_host = "d1qlg1mi5h09s2.cloudfront.net"
+  # config.action_controller.asset_host = "d1qlg1mi5h09s2.cloudfront.net"
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=31536000"
   config.action_dispatch.rack_cache = {
