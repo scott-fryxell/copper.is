d20065e6558bde3b37b52c4cea844f0211d1c2d5
Michael Garriss <mgarriss@gmail.com>
2012-06-11 09:25:50 -0700

    removing backup file, add fan_spec.rb

--- spec/models/fan_spec.rb
+++ spec/models/fan_spec.rb
@@ -1,4 +1,27 @@
 require 'spec_helper'
 
 describe Fan do
+  it 'is valid empty' do
+    Fan.create.should be_valid
+  end
+  
+  describe '#current_order' do
+    it 'creates an Order when there is no current order' do
+      proc do
+        Fan.create.current_order 
+      end.should change(Order,:count).by(1)
+    end
+    
+    it 'the returns an Order is :current state' do
+      Fan.create.current_order.current?.should be_true
+    end
+    
+    it 'finds and returns the current Order is it exists' do
+      user = Fan.create
+      order = user.current_order
+      proc do
+        user.current_order.should eq(order) 
+      end.should change(Order,:count).by(0)
+    end
+  end
 end
