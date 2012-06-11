53c515a2444b970d5c7d851ef43d18e9aecc2ec1
Michael Garriss <mgarriss@gmail.com>
2012-06-11 09:19:37 -0700

    new specs

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Order do
+  it 'must be owned my a user' do
+    Order.create.should_not be_valid
+    User.create.orders.create.should be_valid
+  end
+  
+  it 'attempts to charge a credit card when :process! is called'
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe User do
+  it 'contains at least a Fan or an Author'
+  it 'can contain both'
+end
+
+__END__
+maybe this is just tied together by auth_source....
+auth_sources are first class citizens
+
+
