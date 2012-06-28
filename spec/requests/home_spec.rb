ec10e7d5cbb8150c71f635767795690658d0b466
Scott Fryxell <scott@copper.is>
2012-06-27 19:11:18 -0700

    moving some images around

---
+++ spec/requests/home_spec.rb
@@ -0,0 +1,17 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe "home page experience" do
+  before(:each) do
+    DatabaseCleaner.clean
+    visit "/"
+  end
+
+
+  it "should display the signin ui"
+
+  it "should carosel the sample images"
+
+  it "should let the user sign in with facebook link on connect section"
+
+  it "should show the users recent activity"
+end
