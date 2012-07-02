849bdf1e0ee1b4ffd8a1b4a04873ffd9bda8b458
Scott Fryxell <scott@copper.is>
2012-07-02 13:35:15 -0700

    six and puts for redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,5 @@
-# uri = URI.parse(Copper::Application.config.redistogo_url)
-# REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+put "*************************"
+put Copper::Application.config.redistogo_url
+put "************************"
+uri = URI.parse(Copper::Application.config.redistogo_url)
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
