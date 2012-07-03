3281ef9dd3ffb6787c2e479064758ed0c4101b33
Scott Fryxell <scott@copper.is>
2012-07-03 13:20:52 -0700

    Facebook like integration

--- app/assets/javascripts/home/index.js
+++ app/assets/javascripts/home/index.js
@@ -35,10 +35,13 @@ $(document).on("copper:home_index", function (){
 
   $(document).on("copper:button_installed",  function(event){
     // hide the welcome screen and show congrats
-    $('#welcome').delay(800).slideUp(800);
-    $('#congrats').delay(1600).slideDown(800);
-    $('#facebook').delay(2400).slideDown(800);
-    $('.settings').css("display",'inline-block');
+    $('#welcome').delay(800).fadeOut(800);
+    $('#congrats').delay(1600).fadeIn(800);
+    $('#facebook').delay(1600).fadeIn(800);
+    $('.settings').delay(1600).fadeIn(800, function (){
+      $('.settings').delay(1600).css("display",'inline-block');
+    });
+
   });
 });
 
@@ -47,26 +50,14 @@ $(document).on("copper:home_index:me", function (){
   $('#welcome').show();
 
   if('facebook' == copper.me.identities[0].provider){
-    var likes_url = 'https://graph.facebook.com/' + copper.me.identities[0].uid + '/likes?limit=8&access_token=' + copper.me.identities[0].token;
+    var likes_url = 'https://graph.facebook.com/' + copper.me.identities[0].uid + '/likes?limit=7&access_token=' + copper.me.identities[0].token;
     $.getJSON(likes_url).success(function(facebook) {
-
       $.each(facebook.data,function(i, a_like){
         $.getJSON("http://graph.facebook.com/" + a_like.id).success(function(like){
-          console.debug(like.picture)
-          like.picture
+          var image = $('<img/>', {src:like.picture})
+          $('<a/>', {href:like.link, html:image}).appendTo('#facebook > nav')
         })
       })
     })
   }
 });
-
-
-
-// $.each(data, function(key, val) {
-//   items.push('<li id="' + key + '">' + val + '</li>');
-// });
-//
-// $('<ul/>', {
-//   'class': 'my-new-list',
-//   html: items.join('')
-// }).appendTo('body');
