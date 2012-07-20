d4b06c70d3990c07735d00cf50b46f1ad90f1c66
Scott Fryxell <scott@copper.is>
2012-07-20 12:12:18 -0700

    Revert "remove identities controller"
    This reverts commit 336efd00b5b3984627b602d0bd7ac7f061af2c11.

---
+++ app/controllers/identities_controller.rb
@@ -0,0 +1,28 @@
+class IdentitiesController < ApplicationController
+  filter_resource_access
+
+  def index
+    @identities = current_user.identities
+  end
+
+  def new
+  end
+
+  def show
+    @identity = current_user.identities.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render nothing:true, status:401
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    identity = Identity.find(params[:id])
+    identity.destroy
+    redirect_to identities_path, notice: "Removed that Identity"
+  end
+end
