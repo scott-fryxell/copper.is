87be4c883c4c990d98ba19d8424caa26bda49b59
Scott Fryxell <scott@copper.is>
2012-07-28 19:32:26 -0700

    whitespace nazi

--- app/assets/javascripts/users/show.js
+++ app/assets/javascripts/users/show.js
@@ -1,6 +1,5 @@
 $(document).on("copper:users_show", function (event){
   copper.format_cents_to_dollars("tip_preference_in_cents")
-  var dollars = copper.cents_to_dollars($('#stats > div:nth-child(3) > p').text().trim() );
+  var dollars = copper.cents_to_dollars($('#stats > div:nth-child(3) > p').text().trim());
   $('#stats > div:nth-child(3) > p').text(dollars)
-
 });
