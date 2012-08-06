a307288b2567e14e0c0e91fbd68962cda1cbc6b9
Scott Fryxell <scott@copper.is>
2012-08-06 15:37:52 -0700

    removing old tests and jobs from extras folder

--- extras/discover_identity_job.rb
+++
@@ -1,7 +0,0 @@
-class DiscoverIdentityJob
-  @queue = :high
-  
-  def self.perform page_id
-    Page.find(page_id).discover_identity!
-  end
-end
--- extras/providerable_pages_job.rb
+++
@@ -1,9 +0,0 @@
-class ProviderablePagesJob
-  @queue = :high
-  
-  def self.perform
-    Page.providerable.select(:id).find_each do |page|
-      Resque.enqueue(DiscoverIdentityJob,page.id)
-    end
-  end
-end
