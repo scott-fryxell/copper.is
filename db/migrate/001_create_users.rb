16a7bbbc3f842695e3e999288c9eba7263aa6c1d
Scott Fryxell <scott@copper.is>
2012-10-05 17:11:51 -0700

    adding a space for readability

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
-      t.integer:tip_preference_in_cents,  :null => false, :default => 25
+      t.integer :tip_preference_in_cents,  :null => false, :default => 25
       t.string :email
       t.string :stripe_id
       t.boolean :accept_terms,            :default => false
