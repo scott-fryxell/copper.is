b654637cf5cacf5fab19a5d3943284018f684b05
Scott Fryxell <scott@copper.is>
2012-10-18 15:26:38 -0700

    I guess dashes are bad

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -14,7 +14,7 @@ class TipsController < ApplicationController
   end
 
   def new
-    logger.info "––– referer:#{request.referer}  –––" 
+    logger.info "referer: #{request.referer}"
     
     if current_user 
       render action:'new', layout:false
