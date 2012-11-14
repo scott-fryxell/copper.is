bd5e3d33c122280057bdcca75d77063eb8ad16ba
Scott Fryxell <scott@copper.is>
2012-11-13 18:56:54 -0800

    animation for managing online identities
    nice and slow now.

--- app/assets/javascripts/authors/edit.js
+++ app/assets/javascripts/authors/edit.js
@@ -2,7 +2,12 @@ $(document).on("load.authors_edit", function (){
 
   $("section#identities > header > a").click(function(event){
     event.preventDefault();
-    $('section#identities').addClass('edit')
+    $('section#identities').toggleClass('edit')
   });
+
+  $("section#identities > aside > nav > a > img").click(function(event){
+    $(this).addClass('working')
+  });
+
 
 });
