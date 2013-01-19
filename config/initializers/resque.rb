597cbe20ee720d091ac321ae22600b13c00efb04
Scott Fryxell <scott@copper.is>
2013-01-18 18:01:54 -0800

    adding an after fork for resque.
    getting some weird ass postgrese errors.
    
    http://stackoverflow.com/questions/9961044/postgres-error-on-heroku-with
    -resque

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -4,3 +4,5 @@ Resque.redis = Copper::Application.config.redistogo_url
 Resque::Server.use(Rack::Auth::Basic) do |user,password|
   password = Copper::Application.config.resque_overview_password
 end
+
+Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
