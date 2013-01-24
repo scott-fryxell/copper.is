b3be1a988d0737a7c6dfabc93bcfa2960308149a
Scott Fryxell <scott@copper.is>
2013-01-23 17:06:10 -0800

    Revert "cleaning up authorization rules"
    This reverts commit 1bd5eff18da2bc66da542d279275d71364576db0.

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -13,11 +13,15 @@ authorization do
       if_attribute :id => is { user.id }
     end
     
-    has_permission_on :tips,       :to => [:update, :destroy, :create] 
+    has_permission_on :tips,       :to => [:create]
+    has_permission_on :tips,       :to => [:update, :destroy] do
+      if_attribute :user => is { user }
+    end
 
     has_permission_on :checks,     :to => [:read]
     has_permission_on :orders,     :to => [:read,:update]
     has_permission_on :authors,    :to => [:edit, :destroy]
+    # has_permission_on :pages,      :to => [:index]
     has_permission_on :cards,      :to => [:manage]
   end
 
