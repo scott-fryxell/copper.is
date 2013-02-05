517f2599e282c39f29cca5be0770aee060cc9025
Scott Fryxell <scott@copper.is>
2013-02-04 16:06:23 -0800

    going with a before for for jobs

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,8 +1,6 @@
 require 'resque/server'
-Resque.redis = Copper::Application.config.redistogo_url
-
 Resque::Server.use(Rack::Auth::Basic) do |user,password|
   password = Copper::Application.config.resque_overview_password
 end
-
-Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
+Resque.redis = Copper::Application.config.redistogo_url
+Resque.before_fork = Proc.new { ActiveRecord::Base.establish_connection }
