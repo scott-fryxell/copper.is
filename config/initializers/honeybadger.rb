0b2ca11458d7878e83cd0359ed9698f39e63bbb2
Scott Fryxell <scott@copper.is>
2013-03-12 15:40:18 -0700

    tell honeybadger what keys to ignore

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -1,3 +1,7 @@
 Honeybadger.configure do |config|
   config.api_key = 'ed4d9441'
+  
+  config.params_filters << "stripe_secret, twitter_secret, twitter_oauth_secret,
+                            google_secret, facebook_secret, tumblr_secret, github_secret,
+                            vimeo_secret, soundcloud_secret, flickr_secret, resque_overview_password"
 end
