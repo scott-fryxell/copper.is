729040f41cccbcf26e65a71eb6970a9ef7557094
Scott Fryxell <scott@copper.is>
2013-03-26 15:33:40 -0700

    switch it back to an after_fork
    this is for connection reset by peer error's

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,4 +3,4 @@ Resque::Server.use(Rack::Auth::Basic) do |user,password|
   password = Copper::Application.config.resque_overview_password
 end
 Resque.redis = Copper::Application.config.redistogo_url
-Resque.before_fork = Proc.new { ActiveRecord::Base.establish_connection }
+Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
