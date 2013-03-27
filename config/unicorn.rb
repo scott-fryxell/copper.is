1c3a2e7d9e74fb6cd5ca016a03733b5ea4e23b72
Scott Fryxell <scott@copper.is>
2013-03-26 17:30:21 -0700

    doh. 3 for production!

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-worker_processes 1 # amount of unicorn workers to spin up
+worker_processes 3 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
 preload_app true
 
