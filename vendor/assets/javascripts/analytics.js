928b6f574317b1989e2ddaeab42ce60c2c8f417f
Scott Fryxell <scott@copper.is>
2013-04-16 15:30:09 -0700

    adding analytics for outbound urls

--- vendor/assets/javascripts/analytics.js
+++ vendor/assets/javascripts/analytics.js
@@ -7,3 +7,13 @@ _gaq.push(['_trackPageview']);
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+
+// document.ready
+$(function() {
+  $('body').on('click', 'a[itemprop=url]', function() {
+    console.debug('outbound link');
+    try { 
+      _gaq.push(['_trackEvent', 'Outbound Links', $(this).attr('href')]); 
+    } catch(err){}
+  });
+});
