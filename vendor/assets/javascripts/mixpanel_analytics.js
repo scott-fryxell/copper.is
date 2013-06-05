9d25fe8b6b45c05d94eac5bf9a3550f619f7d930
Scott Fryxell <scott@copper.is>
2013-06-04 17:58:21 -0700

    seperate analytics into vendor files

--- vendor/assets/javascripts/mixpanel_analytics.js
+++ vendor/assets/javascripts/mixpanel_analytics.js
@@ -1,24 +1,6 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-30118836-1']);
-_gaq.push(['_setDomainName', 'copper.is']);
-_gaq.push(['_trackPageview']);
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
-// document.ready
-$(function() {
-  $('body').on('click', 'a[itemprop=url]', function() {
-    console.debug('outbound link');
-    try { 
-      _gaq.push(['_trackEvent', 'Outbound Links', $(this).attr('href')]); 
-    } catch(err){}
-  });
-});
-
 (function(e,b){if(!b.__SV){var a,f,i,g;window.mixpanel=b;a=e.createElement("script");a.type="text/javascript";a.async=!0;a.src=("https:"===e.location.protocol?"https:":"http:")+'//cdn.mxpnl.com/libs/mixpanel-2.2.min.js';f=e.getElementsByTagName("script")[0];f.parentNode.insertBefore(a,f);b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==
 typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");for(g=0;g<i.length;g++)f(c,i[g]);
 b._i.push([a,e,d])};b.__SV=1.2}})(document,window.mixpanel||[]);
-mixpanel.init("5fe27d7605c0463448747d7a5ac3e11b");
+
+// mixpanel.init("5fe27d7605c0463448747d7a5ac3e11b"); production
+mixpanel.init("36697a4951aeffd03bca5a4b81c051d4");  /*development*/
