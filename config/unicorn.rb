e5110961bb9026b6548ac6e75de3ce169f9399d0
Scott Fryxell <scott@copper.is>
2013-07-16 17:11:51 -0700

    bumping to 6
    damn. our memory footprint aint shit.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-worker_processes 5 # amount of unicorn workers to spin up
+worker_processes 6 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
 preload_app true
 
