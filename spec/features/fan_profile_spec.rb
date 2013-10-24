eb752f7a1c8cf6e633df04629b551826597d181c
Scott Fryxell <scott@copper.is>
2013-10-23 20:10:45 -0700

    I'm doing like 17 things here.
    I have kinda screwed this branch.

--- spec/features/fan_profile_spec.rb
+++ spec/features/fan_profile_spec.rb
@@ -1,6 +1,140 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require 'spec_helper'
 
 describe "A Fan", :slow do
+
+  before(:each) do
+    mock_page_and_user
+    visit "/auth/facebook"
+    User.count.should == 1
+
+    VCR.use_cassette('nytimes') do
+      User.first.tip({url:'http://www.nytimes.com', title:'nytimes homepage', amount_in_cents:'50'})
+    end
+    VCR.use_cassette('fasterlighterbetter') do
+      User.first.tip({url:'http://www.fasterlighterbetter.com', title:'faster lighter better', amount_in_cents:'50'})
+    end
+    visit "/"
+  end
+
+  it "can see their name" do
+    page.save_screenshot('tmp/screenshots/profile/01.png')
+    find("#banner > hgroup > p").should have_content("facebook user")
+  end
+
+  it 'can view the number of authors tipped' do
+    User.first.pages.count.should == 2
+    within '#stats > div:nth-child(2) > p' do
+      page.should have_content('2');
+    end
+  end
+
+  it 'can view their average tip amount' do
+    within '#stats > div > p > span' do
+      page.should have_content('0.75');
+    end
+  end
+
+  it 'can increase the default tip amount' do
+    page.find("img[alt='increase default tip amount']").click
+
+    within 'span[itemprop="tip_preference_in_cents"]' do
+      page.should have_content('1');
+    end
+
+    within 'span[itemprop="tip_preference_in_cents"]' do
+      page.should have_content('1');
+    end
+
+    visit '/'
+    within 'span[itemprop="tip_preference_in_cents"]' do
+      page.should have_content('1');
+    end
+    page.find("img[alt='increase default tip amount']").click
+    page.find("img[alt='increase default tip amount']").click
+    within 'span[itemprop="tip_preference_in_cents"]' do
+      page.should have_content('3');
+    end
+
+    page.find("img[alt='decrease default tip amount']").click
+    within 'span[itemprop="tip_preference_in_cents"]' do
+      page.should have_content('2');
+    end
+    page.find("img[alt='increase default tip amount']").click
+    within 'span[itemprop="tip_preference_in_cents"]' do
+      page.should have_content('3');
+    end
+  end
+
+  it 'can reduce the default tip amount' do
+    within 'span[itemprop="tip_preference_in_cents"]' do
+      page.should have_content('0.75');
+    end
+
+    page.find("img[alt='decrease default tip amount']").click
+    within 'span[itemprop="tip_preference_in_cents"]' do
+      page.should have_content('0.50');
+    end
+    visit '/'
+    within 'span[itemprop="tip_preference_in_cents"]' do
+      page.should have_content('0.50');
+    end
+
+    page.find("img[alt='decrease default tip amount']").click
+    page.find("img[alt='decrease default tip amount']").click
+    within 'span[itemprop="tip_preference_in_cents"]' do
+      page.should have_content('0.10');
+    end
+    page.find("img[alt='increase default tip amount']").click
+    within 'span[itemprop="tip_preference_in_cents"]' do
+      page.should have_content('0.25');
+    end
+    page.find("img[alt='decrease default tip amount']").click
+    within 'span[itemprop="tip_preference_in_cents"]' do
+      page.should have_content('0.10');
+    end
+  end
+
+  it 'can see the most recent tip' do
+    page.execute_script('$("section.pages > details:nth-child(2)").attr("open", "open")')
+    sleep 1.5
+    within 'section.pages > details:nth-child(2) > summary > figure > figcaption' do
+      page.should have_content('0.50');
+    end
+    page.save_screenshot('tmp/screenshots/profile/02.png')
+  end
+
+  it 'can see their tipped pages' do
+    page.should have_css('section.pages > details', visible:true, count:2)
+  end
+
+  it 'can cancel a pending tip' do
+    within ('section.pages > details:nth-child(2)') do
+      page.find("input[type=image]").click
+    end
+    page.should have_css('section.pages > details', visible:true, count:1)
+  end
+
+  it 'can change the tip amount' do
+
+    within ('section.pages > details:nth-child(2) > summary') do
+      page.should have_content('0.50')
+    end
+
+    within ('section.pages > details:nth-child(2) details') do
+      fill_in 'amount_in_dollars', with:'2'
+      click_button('Save')
+    end
+
+    within ('section.pages > details:nth-child(2) > summary') do
+      page.should have_content('2')
+    end
+
+    visit '/'
+
+    within ('section.pages > details:nth-child(2) > summary') do
+      page.should have_content('2')
+    end
+  end
 
   describe "right after signing up" do
 
@@ -16,7 +150,7 @@ describe "A Fan", :slow do
       page.execute_script('$(document).trigger("copper_button_installed")')
     end
 
-    it "can provide credit card information", :broken do
+    it "can provide credit card information" do
       page.execute_script('$(document).trigger("copper_button_installed")')
       within("#card") do
         page.should have_css('form', :visible => true)
@@ -34,7 +168,7 @@ describe "A Fan", :slow do
       end
     end
 
-    it "will be told if their credit card info is bad", :vcr, :broken do
+    it "will be told if their credit card info is bad", :vcr do
       page.execute_script('$(document).trigger("copper_button_installed")')
       within("#card") do
         page.should have_css('form', :visible => true)
@@ -54,140 +188,4 @@ describe "A Fan", :slow do
     end
   end
 
-  describe "after tipping some pages" do
-
-    before(:each) do
-      mock_page_and_user
-      visit "/auth/facebook"
-      User.count.should == 1
-
-      # VCR.use_cassette('nytimes') do :broken
-        User.first.tip({url:'http://www.nytimes.com', title:'nytimes homepage', amount_in_cents:'50'})
-      # end
-      # VCR.use_cassette('fasterlighterbetter') do
-        User.first.tip({url:'http://www.fasterlighterbetter.com', title:'faster lighter better', amount_in_cents:'50'})
-      # end
-      visit "/"
-    end
-
-    it "can see their name" do
-      page.save_screenshot('tmp/screenshots/profile/01.png')
-      find("#banner > hgroup > p").should have_content("facebook user")
-    end
-
-    it 'can view the number of authors tipped' do
-      User.first.pages.count.should == 2
-      within '#stats > div:nth-child(2) > p' do
-        page.should have_content('2');
-      end
-    end
-
-    it 'can view their average tip amount' do
-      within '#stats > div > p > span' do
-        page.should have_content('0.75');
-      end
-    end
-
-    it 'can increase the default tip amount' do
-      page.find("img[alt='increase default tip amount']").click
-
-      within 'span[itemprop="tip_preference_in_cents"]' do
-        page.should have_content('1');
-      end
-
-      within 'span[itemprop="tip_preference_in_cents"]' do
-        page.should have_content('1');
-      end
-
-      visit '/'
-      within 'span[itemprop="tip_preference_in_cents"]' do
-        page.should have_content('1');
-      end
-      page.find("img[alt='increase default tip amount']").click
-      page.find("img[alt='increase default tip amount']").click
-      within 'span[itemprop="tip_preference_in_cents"]' do
-        page.should have_content('3');
-      end
-
-      page.find("img[alt='decrease default tip amount']").click
-      within 'span[itemprop="tip_preference_in_cents"]' do
-        page.should have_content('2');
-      end
-      page.find("img[alt='increase default tip amount']").click
-      within 'span[itemprop="tip_preference_in_cents"]' do
-        page.should have_content('3');
-      end
-    end
-
-    it 'can reduce the default tip amount' do
-      within 'span[itemprop="tip_preference_in_cents"]' do
-        page.should have_content('0.75');
-      end
-
-      page.find("img[alt='decrease default tip amount']").click
-      within 'span[itemprop="tip_preference_in_cents"]' do
-        page.should have_content('0.50');
-      end
-      visit '/'
-      within 'span[itemprop="tip_preference_in_cents"]' do
-        page.should have_content('0.50');
-      end
-
-      page.find("img[alt='decrease default tip amount']").click
-      page.find("img[alt='decrease default tip amount']").click
-      within 'span[itemprop="tip_preference_in_cents"]' do
-        page.should have_content('0.10');
-      end
-      page.find("img[alt='increase default tip amount']").click
-      within 'span[itemprop="tip_preference_in_cents"]' do
-        page.should have_content('0.25');
-      end
-      page.find("img[alt='decrease default tip amount']").click
-      within 'span[itemprop="tip_preference_in_cents"]' do
-        page.should have_content('0.10');
-      end
-    end
-
-    it 'can see the most recent tip' do
-      page.execute_script('$("section.pages > details:nth-child(2)").attr("open", "open")')
-      sleep 1.5
-      within 'section.pages > details:nth-child(2) > summary > figure > figcaption' do
-        page.should have_content('0.50');
-      end
-      page.save_screenshot('tmp/screenshots/profile/02.png')
-    end
-
-    it 'can see their tipped pages ' do
-      page.should have_css('section.pages > details', visible:true, count:2)
-    end
-
-    it 'can cancel a pending tip', :broken do
-      within ('section.pages > details:nth-child(2)') do
-        page.find("input[type=image]").click
-      end
-      page.should have_css('section.pages > details', visible:true, count:1)
-    end
-
-    it 'can change the tip amount', :broken do
-
-      within ('section.pages > details:nth-child(2) > summary') do
-        page.should have_content('0.50')
-      end
-
-      within ('section.pages > details:nth-child(2) details') do
-        fill_in 'amount_in_dollars', with:'2'
-        click_button('Save')
-      end
-
-      within ('section.pages > details:nth-child(2) > summary') do
-        page.should have_content('2')
-      end
-
-      visit '/'
-
-      within ('section.pages > details:nth-child(2) > summary') do
-        page.should have_content('2')
-      end
-    end
-  end
 end
