bc672a97e7aa35a302da93614a2a01bcdcdbbfe5
Scott Fryxell <scott@copper.is>
2014-03-08 20:22:11 -0800

    this is the way to seed

--- db/seeds/roles.seeds.rb
+++ db/seeds/roles.seeds.rb
@@ -1,9 +1,7 @@
 puts "creating roles"
-roles = Role.find_or_create_by_name!([
-  { name:'Fan' },
-  { name:'Curator' },
-  { name:'Author' },
-  { name:'Guest' },
-  { name:'Admin' },
-  { name:'God' }
-])
+Role.find_or_create_by_name!('Fan')
+Role.find_or_create_by_name!('Curator')
+Role.find_or_create_by_name!('Author')
+Role.find_or_create_by_name!('Guest')
+Role.find_or_create_by_name!('Admin')
+Role.find_or_create_by_name!('God')
