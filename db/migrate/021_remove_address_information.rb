ba3fd6fc641257183ec71354583cba79aff15a28
Scott Fryxell <scott@copper.is>
2014-03-08 16:32:28 -0800

    remove address from the app

--- db/migrate/021_remove_address_information.rb
+++ db/migrate/021_remove_address_information.rb
@@ -1,5 +1,12 @@
 class RemoveAddressInformation < ActiveRecord::Migration
   def up
+    remove_column :users, :payable_to
+    remove_column :users, :line1
+    remove_column :users, :line2
+    remove_column :users, :city
+    remove_column :users, :postal_code
+    remove_column :users, :subregion_code
+    remove_column :users, :country_code
   end
 
   def down
