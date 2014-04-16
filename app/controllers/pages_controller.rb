241144b8072bc14b59c610d491fdbff0484ad991
Scott Fryxell <scott@copper.is>
2014-04-15 21:57:14 -0700

    Styling changes to tips page.  Adding app cache to tip layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,6 +10,5 @@ class PagesController < ApplicationController
   def appcache
     expires_in 1.second, :public => true
     render action:'appcache', layout:false, content_type:'text/cache-manifest'
-
   end
 end
