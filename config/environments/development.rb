95dcff72dc2842af8dfe13de26151b770d51dd50
Scott Fryxell <scott@copper.is>
2014-05-04 17:40:56 -0700

    auto login in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,4 +20,99 @@ Copper::Application.configure do
   config.active_record.auto_explain_threshold_in_seconds = 0.25
   config.lograge.enabled = true
 
+
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:twitter] = {
+    'provider' => 'twitter',
+    'uid' => '123545',
+    'info' => {
+      'name' => 'copper',
+      'nickname'=> 'copper_is',
+    },
+    'credentials' => {
+      'token' => '666_777_666',
+      'secret' => 'its_a_secret'
+    },
+  }
+  OmniAuth.config.mock_auth[:facebook] = {
+    'provider' => 'facebook',
+    'uid' => '234567',
+    'info' => {
+      'name' => 'facebook user',
+      'nickname'=> 'facebook.user',
+      'email' => 'user@facebook.com'
+    },
+    'credentials' => {
+      'token' => '666_777_666',
+      'secret' => 'its_a_secret'
+    },
+  }
+  OmniAuth.config.mock_auth[:google_oauth2] = {
+    'provider' => 'google_oauth2',
+    'uid' => '234567',
+    'info' => {
+      'name' => 'google user',
+    },
+    'credentials' => {
+      'token' => '666_777_666',
+      'secret' => 'its_a_secret'
+    },
+  }
+  OmniAuth.config.mock_auth[:tumblr] = {
+    'provider' => 'tumblr',
+    'uid' => '234567',
+    'info' => {
+      'name' => 'tumblr user',
+    },
+    'credentials' => {
+      'token' => '666_777_666',
+      'secret' => 'its_a_secret'
+    },
+  }
+  OmniAuth.config.mock_auth[:github] = {
+    'provider' => 'github',
+    'uid' => '3456789',
+    'info' => {
+      'name' => 'github user',
+    },
+    'credentials' => {
+      'token' => '666_777_666',
+      'secret' => 'its_a_secret'
+    },
+  }
+  OmniAuth.config.mock_auth[:vimeo] = {
+    'provider' => 'vimeo',
+    'uid' => '3456789',
+    'info' => {
+      'name' => 'vimeo user',
+    },
+    'credentials' => {
+      'token' => '666_777_666',
+      'secret' => 'its_a_secret'
+    },
+  }
+  OmniAuth.config.mock_auth[:soundcloud] = {
+    'provider' => 'soundcloud',
+    'uid' => '3456789',
+    'info' => {
+      'name' => 'soundcloud user',
+    },
+    'credentials' => {
+      'token' => '666_777_666',
+      'secret' => 'its_a_secret'
+    },
+  }
+  OmniAuth.config.mock_auth[:flickr] = {
+    'provider' => 'flickr',
+    'uid' => '3456789',
+    'info' => {
+      'name' => 'flickr user',
+    },
+    'credentials' => {
+      'token' => '666_777_666',
+      'secret' => 'its_a_secret'
+    },
+  }
+
+
 end
