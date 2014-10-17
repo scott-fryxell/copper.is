9ae9adbd77c9ae64c0a1a5f84d83255ffce6152a
Scott Fryxell <scott@copper.is>
2014-10-17 15:10:40 -0700

    use redid for eventing puss pull

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
 uri = URI.parse(Copper::Application.config.redistogo_url)
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+$eventer = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
