601010417d4ca2bd15ec08127595965943ed3ddb
Scott Fryxell <scott@copper.is>
2014-10-19 22:26:38 -0700

    reconnecting on fork
    This code does not feel 100%

--- config/puma.rb
+++ config/puma.rb
@@ -3,13 +3,12 @@ require 'resque/server'
 workers Integer(ENV['PUMA_WORKERS'] || 2)
 threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
 
-preload_app!
-
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
+  ActiveRecord::Base.connection_pool.disconnect!
   # worker specific setup
   ActiveSupport.on_load(:active_record) do
     config = ActiveRecord::Base.configurations[Rails.env] ||
@@ -19,8 +18,8 @@ on_worker_boot do
   end
 
   if defined?(Resque)
-    uri = URI.parse(Copper::Application.config.redistogo_url)
-    Resque.redis = Redis.new(host:uri.host, port:uri.port, password:uri.password)
+    # uri = URI.parse(Copper::Application.config.redistogo_url)
+    # Resque.redis = Redis.new(host:uri.host, port:uri.port, password:uri.password)
 
     Redis.current.client.reconnect
     $eventer = Redis.current
