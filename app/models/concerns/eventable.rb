b41a074b8aa7317537213c84bd7ac1cb40329e73
Scott Fryxell <scott@copper.is>
2014-10-19 22:37:29 -0700

    no need for json
    just send the id back. client will update the cache and can get what it
    needs from ajaxing (which will be local) the page and using the item
    spec
    
    this is in keeping with pub/sub just being the messenger. payloads will
    come from the pages themselves
    
    https://html.spec.whatwg.org/multipage/microdata.html

--- app/models/concerns/eventable.rb
+++ app/models/concerns/eventable.rb
@@ -4,15 +4,15 @@ module Eventable
   included do
 
     after_create do |object|
-      $eventer.publish("#{object.class.name.downcase}.create", object.to_json)
+      $eventer.publish("#{object.class.name.downcase}.create", object.id)
     end
 
     after_save do |object|
-      $eventer.publish("#{object.class.name.downcase}.save", object.to_json)
+      $eventer.publish("#{object.class.name.downcase}.save", object.id)
     end
 
     after_destroy do |object|
-      $eventer.publish("#{object.class.name.downcase}.destroy", object.to_json)
+      $eventer.publish("#{object.class.name.downcase}.destroy", object.id)
     end
 
   end
