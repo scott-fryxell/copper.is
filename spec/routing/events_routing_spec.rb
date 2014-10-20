21ed109a56635a3c598c240b0c63357e76fb85ba
Scott Fryxell <scott@copper.is>
2014-10-19 22:50:30 -0700

    fix routing test

--- spec/routing/events_routing_spec.rb
+++ spec/routing/events_routing_spec.rb
@@ -5,18 +5,9 @@ describe EventsController do
 
   describe "routing" do
 
-    it "routes to #domain" do
-      # get("/events").should route_to("events#domain")
-    end
-
     # TODO: how do you write this test as a concern?
-    it "routes to #collection" do
-      get("/events").should route_to("events#collection")
-    end
-
-    # TODO: how do you write this test as a concern?
-    it "routes to #member" do
-      # get("/events/1").should route_to("events#member")
+    it "routes to #publisher" do
+      get("/events").should route_to("events#publisher")
     end
 
   end
