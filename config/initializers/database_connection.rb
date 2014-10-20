c92c134b6f148c9f2f4d2c151e090807cf804c88
Scott Fryxell <scott@copper.is>
2014-10-19 22:43:26 -0700

    turn tests back on

--- config/initializers/database_connection.rb
+++ config/initializers/database_connection.rb
@@ -10,4 +10,12 @@ Rails.application.config.after_initialize do
     end
   end
 
+  if defined?(Resque)
+    # uri = URI.parse(Copper::Application.config.redistogo_url)
+    # Resque.redis = Redis.new(host:uri.host, port:uri.port, password:uri.password)
+
+    Redis.current.client.reconnect
+    $eventer = Redis.current
+
+  end
 end
