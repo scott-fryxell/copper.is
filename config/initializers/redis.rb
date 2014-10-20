0d9113ce1c1f6434b8b8f549ff7e21d81c98aca8
Scott Fryxell <scott@copper.is>
2014-10-19 22:16:17 -0700

    shit way to manage redis connections
    have to ping reds to get it to reset connections

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,15 @@
-uri = URI.parse(Copper::Application.config.redistogo_url)
-Resque.redis = Redis.new(host:uri.host, port:uri.port, password:uri.password)
-$eventer = Redis.current
+uri =      URI.parse(Copper::Application.config.redistogo_url)
+$eventer = Redis.new(host:uri.host, port:uri.port, password:uri.password)
+
+heartbeat_thread = Thread.new do
+  while true
+    $eventer.publish "heartbeat","thump"
+    sleep 10.seconds
+  end
+end
+
+at_exit do
+  # not sure this is needed, but just in case
+  heartbeat_thread.kill
+  $eventer.quit
+end
