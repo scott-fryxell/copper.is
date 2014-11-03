c68738799f5beb6ef1c3a9b9d28d15af0c6a805c
Scott Fryxell <scott@copper.is>
2014-11-03 12:57:00 -0800

    tighten up migrations

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -1,11 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.integer :tip_preference_in_cents,  :null => false, :default => 100
-      t.string :email
+      t.string  :name
+      t.integer :tip_preference_in_cents,  :null => false, :default => 50
+      t.string  :email
+
       t.string :stripe_id
       t.boolean :accept_terms,            :default => false
+      t.boolean :share_on_facebook,       :default => false
       t.timestamps
     end
     add_index :users, :email
--- db/migrate/017_add_share_on_facebook_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddShareOnFacebookToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :share_on_facebook, :boolean
-  end
-end
