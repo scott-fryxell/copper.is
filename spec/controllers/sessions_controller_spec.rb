526c0a5521308f73d3c87343ca71aa9e03bc6352
Scott Fryxell <scott@copper.is>
2014-11-13 14:05:29 -0800

    better session tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,27 +1,28 @@
 require 'spec_helper'
 
 describe SessionsController, :type => :controller do
+  let(:me) { create!(:user) }
+
   before :each do
-    me_setup
+    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
   end
 
-  it 'should create a new account', :vcr do
-    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
-    expect(User).to receive(:create_with_omniauth).and_return(create!(:user))
-    get :create, provider:"facebook"
+  it 'should create a new account' do
+    expect(User).to receive(:create_with_omniauth).and_return(me)
+    get :create, provider:'facebook'
   end
 
   it 'create a second account' do
-    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
-    get_with @me, :create, provider:"twitter"
+    # expect(User).to receive(:create_with_omniauth).and_return(me)
+    get_with me, :create, provider:"twitter", uid:'brokenbydawn'
   end
 
   it 'Should log the user out' do
-    delete_with @me, :destroy
+    delete_with me, :destroy
   end
 
   it 'Should log the user out' do
-    delete_with @me, :destroy, redirect_to:"/trending"
+    delete_with me, :destroy, redirect_to:"/trending"
   end
 
   it 'handle a login failure'
