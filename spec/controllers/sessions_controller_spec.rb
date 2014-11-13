e14dfbd831f9bbc7e7d63198d0fe956b00bfcf39
Scott Fryxell <scott@copper.is>
2014-11-13 04:46:30 -0800

    testing happy path login

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -6,14 +6,26 @@ describe SessionsController, :type => :controller do
     request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
   end
 
-  it 'should let them log in' do skip
-    expect(controller.session["omniauth.auth"][:uid]).to eq('234567')
-    post :create
+  it 'should create a new account' do
+    # expect(controller.session["omniauth.auth"][:uid]).to eq('234567')
+    expect(User).to receive(:create_with_omniauth).and_return(create!(:user))
+    get :create, provider:"facebook"
+  end
+
+  it 'create a second account' do
+    # expect(User).to receive(:create_with_omniauth).and_return(create!(:user))
+    get_with @me, :create, provider:"facebook"
   end
 
   it 'Should log the user out' do
     delete_with @me, :destroy
   end
+
+  it 'Should log the user out' do
+    delete_with @me, :destroy, redirect_to:"/trending"
+  end
+
+
 
   it 'handle a login failure' do
     post_with @me, :failure
