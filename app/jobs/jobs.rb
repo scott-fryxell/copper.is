599f8a599f0a9ab357ef96ac10521a4fca452feb
Scott Fryxell <scott@copper.is>
2014-11-29 15:17:12 -0800

    switching to normal que
    will need to refactor around scheduling

--- app/jobs/jobs.rb
+++ app/jobs/jobs.rb
@@ -1,5 +1,12 @@
-class UsersJob
-  @queue = :high
+
+# TODO: this is where i tie all the gears together.
+# once scheduled these jobs cycle through the service
+# billing fans for their tips
+# finding authorse to invite to the service
+# paying authors that have proviced their bank info
+
+class PayArtistsJob
+  @queue = :normal
   def self.perform
     User.select(:id).find_each do |user|
       Resque.enqueue user.class, user.id, :try_to_create_check!
@@ -8,7 +15,7 @@ class UsersJob
 end
 
 class EarnedChecksJob
-  @queue = :high
+  @queue = :normal
   def self.perform
     Check.earned.select(:id).find_each do |check|
       # Resque.enqueue check.class, check.id, :message_author!
@@ -16,8 +23,8 @@ class EarnedChecksJob
   end
 end
 
-class WantedAuthorsJob
-  @queue = :high
+class InviteAuthorsJoin
+  @queue = :normal
   def self.perform
     Author.wanted.select(:id).find_each do |wanted|
       Resque.enqueue wanted.class, wanted.id, :message_wanted!
@@ -26,7 +33,7 @@ class WantedAuthorsJob
 end
 
 class StrangerAuthorsJob
-  @queue = :high
+  @queue = :normal
   def self.perform
     Author.strangers.select(:id).find_each do |non_user|
       Resque.enqueue non_user.class, non_user.id, :try_to_add_to_wanted_list!
