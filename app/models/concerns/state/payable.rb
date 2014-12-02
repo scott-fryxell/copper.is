900fc39ccee7fa35cc4f298a9aaebb74d6a519a5
Scott Fryxell <scott@copper.is>
2014-12-01 17:33:54 -0800

    is a payable concern

--- app/models/concerns/state/payable.rb
+++ app/models/concerns/state/payable.rb
@@ -3,6 +3,14 @@ module State
     extend ActiveSupport::Concern
 
     included do
+      validates :order, presence:true
+      validate :validate_only_being_added_to_current_order, :on => :create
+
+      before_destroy do |tip|
+        tip.page.touch
+        raise CantDestroyException unless tip.promised?
+      end
+
 
       scope :promised,   -> { where(paid_state:'promised') }
       scope :charged,    -> { where(paid_state:'charged') }
@@ -30,6 +38,25 @@ module State
       end
 
     end
+
+    def validate_only_being_added_to_current_order
+      unless self.order.current?
+        errors.add(:order_id,"can only add a tip to a current order")
+      end
+    end
+
+    def validate_presence_of_paid_order
+      unless self.order.paid?
+        errors.add(:order_id, "tip order must be :paid for :charged tips")
+      end
+    end
+
+    def validate_presence_of_check
+      unless self.check_id
+        errors.add(:check_id, "check_id must not be null for :kinged tips")
+      end
+    end
+
 
   end
 end
--- app/models/tip.rb
+++ app/models/tip.rb
@@ -23,35 +23,7 @@ class Tip < ActiveRecord::Base
   validates_associated :page
 
   validates :page, presence:true
-
-  validates :order, presence:true
-
   validates :amount_in_cents, presence:true
-  validate :validate_only_being_added_to_current_order, :on => :create
-
-  before_destroy do |tip|
-    tip.page.touch
-    raise CantDestroyException unless tip.promised?
-  end
-
-
-  def validate_only_being_added_to_current_order
-    unless self.order.current?
-      errors.add(:order_id,"can only add a tip to a current order")
-    end
-  end
-
-  def validate_presence_of_paid_order
-    unless self.order.paid?
-      errors.add(:order_id, "tip order must be :paid for :charged tips")
-    end
-  end
-
-  def validate_presence_of_check
-    unless self.check_id
-      errors.add(:check_id, "check_id must not be null for :kinged tips")
-    end
-  end
 
   def amount_in_dollars
     amount = Float(self.amount_in_cents) / 100
