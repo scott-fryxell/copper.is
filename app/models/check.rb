77179ab819c3289c45412ea3dfaa677376e3d5bb
Scott Fryxell <scott@copper.is>
2014-12-08 19:29:46 -0800

    rename states for check

--- app/models/check.rb
+++ app/models/check.rb
@@ -2,29 +2,24 @@ class Check < ActiveRecord::Base
   include Enqueueable
   include Historicle
   include Message::Payable
-  
+
   belongs_to :user
   has_many :tips, :as => :royalties
 
   validates :user, presence:true
 
-  scope :earned, -> { where(check_state:'earned') }
-  scope :paid,   -> { where(check_state:'paid')   }
-  scope :cashed, -> { where(check_state:'cashed') }
+  scope :earned,      -> { where(check_state:'earned') }
+  scope :deposited,   -> { where(check_state:'deposited') }
+  scope :cashed,      -> { where(check_state:'cashed') }
 
   state_machine :check_state, :initial => :earned do
+
     event :deliver do
-      transition :earned => :paid
+      transition :earned => :deposited
     end
 
     event :reconcile do
-      transition :paid => :cashed
-    end
-
-    state :earned do
-    end
-
-    state :earned do
+      transition :deposited => :cashed
     end
 
   end
